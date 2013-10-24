d1c005f20aea7bb2c7137034dbf3131723c51e34
juan <juanvaccarezza@gmail.com>
2013-10-23 21:28:23 -0300

    Add time tracking and 2 states

--- AVR1284Regador/src/main.c
+++ AVR1284Regador/src/main.c
@@ -2,14 +2,59 @@
 #include <util/delay.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "avr/interrupt.h"
 #include "lcd/lcd.h"
 #include "buttons/buttons.h"
 #include "state/StateMachine.h"
 #include "state/State.h"
-#include "avr/interrupt.h"
 Transition state2Loop(void* data);
 Transition state1Loop(void * data);
 void printStatus(ButtonsStatus butStat);
+
+//100 ms
+uint8_t cms = 0;
+uint8_t seconds = 0;
+uint8_t minutes = 0;
+uint8_t hours = 0;
+uint8_t days = 0;
+
+void initTimer() {
+	TCCR1B |= (1 << WGM12); // Configure timer 1 for CTC mode
+
+	TIMSK1 |= (1 << OCIE1A); // Enable CTC interrupt
+
+	OCR1A = 3124; // Set CTC compare value 100ms, with a prescaler of 256
+
+	TCCR1B |= ((1 << CS12)); // Start timer at 8Mhz/256
+
+	sei();
+	//  Enable global interrupts
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	cms++;
+	if (cms == 10) {
+		seconds++;
+		cms = 0;
+	}
+
+	if (seconds == 60) {
+		seconds = 0;
+		minutes++;
+	}
+	if (minutes == 60) {
+		minutes = 0;
+		hours++;
+	}
+	if (hours == 24) {
+		hours = 0;
+		days++;
+	}
+	char buff[12];
+	snprintf(buff, 12, "%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
+	LCDWriteStringXY(0, 1, (const char *)buff);
+}
 
 Transition state1Loop(void* data) {
 	State state2;
@@ -21,15 +66,12 @@ Transition state1Loop(void* data) {
 	State_new(&state2, &state2Loop);
 	state2Transition.dataFornextState = 0;
 	state2Transition.nextState = &state2;
-
-	LCDClear();
-	LCDWriteString("State1 (EUDB):");
+	_delay_ms(5);
+	LCDWriteStringXY(0,0,"State1");
 
 	while (1) {
 		readButtons(&currentButtonStatus);
-		_delay_ms(5);
 		if (currentButtonStatus.buttons != previousButtonStatus.buttons) {
-			printStatus(currentButtonStatus);
 			previousButtonStatus.buttons = currentButtonStatus.buttons;
 			if (currentButtonStatus.button.enter) {
 				LCDWriteStringXY(7, 1, "1");
@@ -50,15 +92,13 @@ Transition state2Loop(void * data) {
 	State_new(&state1, &state1Loop);
 	state1Transition.dataFornextState = 0;
 	state1Transition.nextState = &state1;
-
-	LCDClear();
-	LCDWriteString("State2 (EUDB):");
+	_delay_ms(5);
+	LCDWriteStringXY(0,0,"State2");
 
 	while (1) {
 		readButtons(&currentButtonStatus);
-		_delay_ms(5);
+
 		if (currentButtonStatus.buttons != previousButtonStatus.buttons) {
-			printStatus(currentButtonStatus);
 			previousButtonStatus.buttons = currentButtonStatus.buttons;
 			if (currentButtonStatus.button.back) {
 				LCDWriteStringXY(8, 1, "1");
@@ -105,6 +145,7 @@ int main(void) {
 //Initialize LCD module
 	LCDInit(LS_BLINK | LS_ULINE);
 
+	initTimer();
 	State_new(&state1, &state1Loop);
 
 	previousButtonStatus.buttons = 0;
@@ -113,15 +154,15 @@ int main(void) {
 	StateMachine_start(&stateMachine, 0);
 
 //Clear the screen
-	LCDClear();
-	LCDWriteString("PRESSED (EUDB):");
+
 
 	while (1) {
-		readButtons(&currentButtonStatus);
-		if (currentButtonStatus.buttons != previousButtonStatus.buttons) {
-			printStatus(currentButtonStatus);
-			previousButtonStatus.buttons = currentButtonStatus.buttons;
-		}
+
+//		readButtons(&currentButtonStatus);
+//		if (currentButtonStatus.buttons != previousButtonStatus.buttons) {
+//			printStatus(currentButtonStatus);
+//			previousButtonStatus.buttons = currentButtonStatus.buttons;
+//		}
 	}
 
 }
