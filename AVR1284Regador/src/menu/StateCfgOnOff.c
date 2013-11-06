9ccd6308253f606fec92f87e6434f49e69c0889a
juan <juanvaccarezza@gmail.com>
2013-11-06 20:52:57 -0300

    StateCfgOnOf implementation

---
+++ AVR1284Regador/src/menu/StateCfgOnOff.c
@@ -0,0 +1,71 @@
+/*
+ * StateCfgOnOff.c
+ *
+ *  Created on: Nov 5, 2013
+ *      Author: juan
+ */
+
+#include "StateCfgOnOff.h"
+#include "../lcd/lcd.h"
+#include <stddef.h>
+#include <stdio.h>
+#include "../buttons/buttons.h"
+
+Transition StateCfgOnOff_cfgOnOff(void * instance, void * data) {
+	Transition transition;
+	ButtonsStatus buttonStatus;
+	StateCfgOnOff* this = instance;
+
+	if (this->shouldPrint) {
+		this->lastSelectedValue = *this->variable;
+		//Force print the last selected value.
+		this->lastPrintValue = this->lastSelectedValue + 1;
+	}
+
+	StateCfgOnOff_updateScreen(this);
+	transition.nextState = &(this->super);
+	transition.dataFornextState = NULL;
+
+	this->shouldPrint = false;
+
+	readButtons(&buttonStatus);
+	if (buttonStatus.button.enter) {
+		*this->variable = this->lastSelectedValue;
+		this->shouldPrint = true;
+		transition = *this->returnTransition;
+	} else if (buttonStatus.button.back) {
+		this->shouldPrint = true;
+		transition = *this->returnTransition;
+	} else if (buttonStatus.button.down || buttonStatus.button.up) {
+		StateCfgOnOff_swapValue(this);
+	}
+
+	return transition;
+}
+
+void StateCfgOnOff_swapValue(StateCfgOnOff * this) {
+	this->lastSelectedValue = !this->lastSelectedValue;
+}
+
+void StateCfgOnOff_new(StateCfgOnOff * this, char* label, bool * variable, Transition * returnTransition) {
+	this->label = label;
+	this->variable = variable;
+	this->returnTransition = returnTransition;
+	this->shouldPrint = true;
+	this->lastSelectedValue = *variable;
+	State_new(&(this->super), this, &StateCfgOnOff_cfgOnOff);
+}
+
+void StateCfgOnOff_updateScreen(StateCfgOnOff* this) {
+	if (this->shouldPrint) {
+		LCDClear();
+		LCDWriteStringXY(0, 0, this->label);
+	}
+
+	if (this->lastSelectedValue != this->lastPrintValue) {
+		char buff[17];
+		snprintf(buff, 17, "%16s", *this->variable ? "On" : "Off");
+		LCDWriteStringXY(0, 1, (const char *) buff);
+		this->lastPrintValue = this->lastSelectedValue;
+	}
+}
---
+++ AVR1284Regador/src/menu/StateCfgOnOff.h
@@ -0,0 +1,33 @@
+/*
+ * StateCfgOnOff.h
+ *
+ *  Created on: Nov 5, 2013
+ *      Author: juan
+ */
+
+#ifndef STATECFGONOFF_H_
+#define STATECFGONOFF_H_
+#include <stdbool.h>
+#include "../state/State.h"
+
+typedef struct {
+	State super;
+	char * label;
+	bool * variable;
+	Transition * returnTransition;
+	bool shouldPrint;
+	bool lastSelectedValue;
+	bool lastPrintValue;
+} StateCfgOnOff;
+
+typedef struct {
+} StateCfgOnOffTransitionData;
+
+Transition StateCfgOnOff_cfgNumber(void * instance, void * data);
+
+void StateCfgOnOff_new(StateCfgOnOff * this, char* label, bool * variable, Transition * returnTransition);
+
+void StateCfgOnOff_updateScreen(StateCfgOnOff* instance);
+
+void StateCfgOnOff_swapValue(StateCfgOnOff * this);
+#endif /* STATECFGONOFF_H_ */
